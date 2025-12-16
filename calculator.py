c371268aed7918f5574d9cb76b374094d52c0a50
Sumathi3355 <sumathi.s2023@vitstudent.ac.in>
2025-12-16 13:51:40 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
